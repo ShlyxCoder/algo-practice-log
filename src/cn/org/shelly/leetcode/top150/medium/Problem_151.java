0df9257663af3315556625a2a42dee75b4c6e3fb
ShlyxCoder <40505282@qq.com>
2025-10-08 00:06:38 +0800

    feat: submit algorithm problem of 1.7

--- src/cn/org/shelly/leetcode/top150/medium/Problem_151.java
+++ src/cn/org/shelly/leetcode/top150/medium/Problem_151.java
@@ -1,16 +1,19 @@
 package cn.org.shelly.leetcode.top150.medium;
 
 public class Problem_151 {
-    class Solution {
-        public String reverseWords(String s) {
-            String[] split = s.split(" ");
-            StringBuilder sb = new StringBuilder();
+        class Solution {
+            public String reverseWords(String s) {
+                String[] split = s.split(" ");
+                StringBuilder sb = new StringBuilder();
 
-            for (int i = split.length - 1; i >=0; i--) {
-                if(split[i].contains(" ")) continue;
-                sb.append(split[i]);
+                for (int i = split.length - 1; i >=0; i--) {
+
+                    sb.append(split[i]);
+                    if(sb.charAt(sb.length() - 1) != ' ') sb.append(" ");
+
+
+                }
+                return sb.toString().trim();
             }
-            return sb.toString();
         }
-    }
 }
