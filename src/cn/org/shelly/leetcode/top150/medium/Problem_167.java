d6d9c49024b4731cdb17d0f4132410223bc8c925
ShlyxCoder <40505282@qq.com>
2025-11-19 17:04:22 +0800

    feat: submit algorithm code of 11.19

---
+++ src/cn/org/shelly/leetcode/top150/medium/Problem_167.java
@@ -0,0 +1,26 @@
+package cn.org.shelly.leetcode.top150.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem_167 {
+
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0, right = numbers.length - 1;
+            while (left < right) {
+                int sum = numbers[left] + numbers[right];
+                if (sum == target) {
+                    return new int[]{left + 1, right + 1};
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return new int[]{-1, -1};
+        }
+    }
+
+
+}
